62be905fb2af68e76d611d968f190e0e2d310062
Marek Doupovec <DoupovecMarek@seznam.cz>
2021-04-08 20:46:58 +0200

    Přidány errory a printy změněny na returny

--- src/basic-arithmetic.py
+++ src/basic-arithmetic.py
@@ -1,58 +1,61 @@
 import math
 
-def ADD (x, y):
-    if isinstance(x,(int,float)) and isinstance(y,(int,float)):
+def ADD(x, y):
+    if (isinstance(x,(int,float))) and (isinstance(y,(int,float))):
         result = x + y
-        print(result)
+        return result
     else:
-        print("Invalid input")
+        raise TypeError("Invalid input")
 
-def SUB (x, y):
-    if isinstance(x,(int,float)) and isinstance(y,(int,float)):
+def SUB(x, y):
+    if (isinstance(x,(int,float))) and (isinstance(y,(int,float))):
         result = x - y
-        print(result)
+        return result
     else:
-        print("Invalid input")
+        raise TypeError("Invalid input")
 
-def MUL (x, y):
-    if isinstance(x,(int,float)) and isinstance(y,(int,float)):
+def MUL(x, y):
+    if (isinstance(x,(int,float))) and (isinstance(y,(int,float))):
         result = x * y
-        print(result)
+        return result
     else:
-        print("Invalid input")
+        raise TypeError("Invalid input")
 
-def DIV (x, y):
-    if isinstance(x,(int,float)) and isinstance(y,(int,float)):
+def DIV(x, y):
+    if (isinstance(x,(int,float))) and (isinstance(y,(int,float))):
         if (x != 0) and (y != 0):
             result = x / y
-            print(result)
+            return result
         else:
-            print("Division by 0")
+            raise ZeroDivisionError("Can't divide by zero")
     else:
-        print("Invalid input")
+        raise TypeError("Invalid input")
 
-def POW (x, y):
-    if isinstance(x,(int,float)) and isinstance(y,(int,float)):
+def POW(x, y):
+    if (isinstance(x,(int,float))) and (isinstance(y,(int,float))):
         result = (x ** y)
-        print(result)
+        return result
     else:
-        print("Invalid input")
+        raise TypeError("Invalid input")
 
-def SQRT (n): #square root
-    if isinstance(n,(int,float)) and isinstance(n,(int,float)):
+def SQRT(n): #square root
+    if (isinstance(n,(int,float))) and (isinstance(n,(int,float))):
         result = math.sqrt(n)
-        print(result)
+        return result
     else:
-        print("Invalid input")
+        raise TypeError("Invalid input")
 
-def ROOT (x, y): #nth root
-    if isinstance(x,(int,float)) and isinstance(y,(int,float)):
-        if (x < 0):
-            x = abs(x)
-            result = pow(x, 1/y) * (-1)
-            print(result)
+def ROOT(x, y): #nth root
+    if (isinstance(x,(int,float))) and (isinstance(y,(int,float))):
+        if x < 0:
+            if y % 2 == 1:
+                x = abs(x)
+                result = pow(x, 1/y) * (-1)
+                return result
+            else:
+                ValueError("Can't root negative number with postivive exponent")
         else:
             result = pow(x, 1/y)
-            print(result)
+            return result
     else:
-        print("Invalid input")
+        raise TypeError("Invalid input")
