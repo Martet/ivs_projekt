a67bb93d201c3917bb0cb1a6c88af6a5ff1ead09
Martet <martin@zmitko.cz>
2021-04-08 20:07:17 +0200

    Opravene jmena testu, exceptions

--- src/test.py
+++ src/test.py
@@ -1,25 +1,26 @@
 import unittest
-import math_library
+from math_library import *
+import math
 
 
 
 class TestADD(unittest.TestCase):
-   def ADDnuber(self):
-      result=ADD(1.00012, 1.00012 )
+   def testADDnuber(self):
+      result=ADD(1.00012, 1.00012)
       self.assertEqual(result, 1.00024)
-      result=ADD(1000000000000, 1000000000000 )
+      result=ADD(1000000000000, 1000000000000)
       self.assertEqual(result, 2000000000000)
       result=ADD(5,-7)
       self.assertEqual(result,-2)
       result=ADD(0,12)
       self.assertEqual(result, 12)
-   def ADDstring(self):
-      self.assertRaises(wronginput,ADD("ad",6))
-      self.assertRaises(wronginput,ADD(6,"ad"))
-      self.assertRaises(wronginput,ADD("ad","ad"))
+   def testADDstring(self):
+      self.assertRaises(TypeError, ADD("ad",6))
+      self.assertRaises(TypeError, ADD(6,"ad"))
+      self.assertRaises(TypeError, ADD("ad","ad"))
 
 class TestSUB(unittest.TestCase):        
-   def SUBnumber(self):
+   def testSUBnumber(self):
       result=SUB(1.00012, 1.00011 )
       self.assertEqual(result, 0.00001)
       result=SUB(1000000000000, 100000000000 )
@@ -28,13 +29,13 @@ class TestSUB(unittest.TestCase):
       self.assertEqual(result,12)
       result=SUB(0,12)
       self.assertEqual(result, -12)
-   def SUBstring(self):
-      self.assertRaises(wronginput,SUB("ad",6))
-      self.assertRaises(wronginput,SUB(6,"ad"))
-      elf.assertRaises(wronginput,SUB("ad","ad"))
+   def testSUBstring(self):
+      self.assertRaises(TypeError, SUB("ad",6))
+      self.assertRaises(TypeError, SUB(6,"ad"))
+      self.assertRaises(TypeError, SUB("ad","ad"))
 
 class TestDIV(unittest.TestCase):
-   def DIVnumber(self):
+   def testDIVnumber(self):
       result=DIV(5000, 5)
       self.assertEqual(result, 1000)
       result=DIV(10, 3)
@@ -45,14 +46,14 @@ class TestDIV(unittest.TestCase):
       self.assertEqual(result, -65)
       result=DIV(-10, -2)
       self.assertEqual(result, 5)
-      self.assertRaises(wronginput,DIV(10,0))
-   def DIVstring(self):
-      self.assertRaises(wronginput,DIV("ad",6))
-      self.assertRaises(wronginput,DIV(6,"ad"))
-      self.assertRaises(wronginput,DIV("ad","ad"))
+      self.assertRaises(ZeroDivisionError, DIV(10,0))
+   def testDIVstring(self):
+      self.assertRaises(TypeError, DIV("ad",6))
+      self.assertRaises(TypeError, DIV(6,"ad"))
+      self.assertRaises(TypeError, DIV("ad","ad"))
 
 class TestSQRT(unittest.TestCase):
-   def SQRTnumber(self):
+   def testSQRTnumber(self):
       result=SQRT(25)
       self.assertEqual(result, 5)
       result=SQRT(0)
@@ -63,12 +64,12 @@ class TestSQRT(unittest.TestCase):
       self.assertEqual(result, math.sqrt(135429.123456))
       result=SQRT(10000000000000000)
       self.assertEqual(result, math.sqrt(10000000000000000))
-      self.assertRaises(wronginput,SQRT(-5))
-      self.assertRaises(wronginput,SQRT(-459))
-   def SQRTstring(self):
-      self.assertRaises(wronginput,SQRT("ad"))
+      self.assertRaises(ValueError, SQRT(-5))
+      self.assertRaises(ValueError, SQRT(-459))
+   def testSQRTstring(self):
+      self.assertRaises(TypeError, SQRT("ad"))
 class TestMUL(unittest.TestCase):
-   def MULnumber(self):
+   def testMULnumber(self):
       result=MUL(10, 10)
       self.assertEqual(result, 100)
       result=MUL(1.23, 1.54)
@@ -81,24 +82,24 @@ class TestMUL(unittest.TestCase):
       self.assertEqual(result, -100)
       result=MUL(-10, -10)
       self.assertEqual(result, 100)
-   def MULstring(self):
-      self.assertRaises(wronginput,MUL("ad",6))
-      self.assertRaises(wronginput,MUL(6,"ad"))
-      self.assertRaises(wronginput,MUL("ad","ad"))
+   def testMULstring(self):
+      self.assertRaises(TypeError, MUL("ad", 6))
+      self.assertRaises(TypeError, MUL(6, "ad"))
+      self.assertRaises(TypeError, MUL("ad", "ad"))
 
 class TestPOW(unittest.TestCase):
-   def POWnumber(self):
-      result=POW(1)
+   def testPOWnumber(self):
+      result = POW(1)
       self.assertEqual(result, 1*1)
-      result=POW(2)
+      result = POW(2)
       self.assertEqual(result, 2*2)
-      result=POW(50)
+      result = POW(50)
       self.assertEqual(result, 50*50)
       result=POW(-20)
       self.assertEqual(result, -20*-20)
 
-   def POWstring(self):
-      self.assertRaises(wronginput,POW("ad")
+   def testPOWstring(self):
+      self.assertRaises(TypeError, POW("ad"))
 
 if __name__ == '__main__':
    unittest.main()
