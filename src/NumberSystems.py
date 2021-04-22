881aaa2c91aef3eff77ceddb71b95960bee2b8ee
bleskounek <danielvopalensky@outlook.cz>
2021-04-22 18:25:36 +0200

    upraveni funkce pouze pro dvojkovou soustavu

--- src/NumberSystems.py
+++ src/NumberSystems.py
@@ -1,16 +1,10 @@
-def NUMSYS(num, before, after):
-    num=int(num,before)
-    finish=[]
+def NUMSYS(num):
+    sys=2
+    result=[]
     while num > 0:
-        finish.insert(0,num%after)
-        num=num/after
-    dictionary={10:"A",11:"B",12:"C",13:"D",14:"E",15:"F",16:"G"}
-    for x in range(len(finish)):
-        if dictionary.has_key(finish[x]):
-            finish[x]=dictionary[finish[x]]
-        else:
-            finish[x]=str(finish[x])
-    return "".join(finish)
+        result.insert(0,cislo%sys)
+        num=num/sys
+    return "".join(result)
 def TO10(num, sys):
     if sys>0:
         int(num,sys)
