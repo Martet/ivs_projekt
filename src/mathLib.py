cc4b69c8c9cf4b688ef57c98e545a6dc4dbb1115
Marek Doupovec <DoupovecMarek@seznam.cz>
2021-04-29 13:51:24 +0200

    Upravene odsazeni tykajici se komentaru

--- src/mathLib.py
+++ src/mathLib.py
@@ -1,12 +1,20 @@
 import math
 import random
 
+"""!
+    @brief Library dedicated to basic arithmetical functions
+
+    Mathematical library containing ADD, SUB, MUL, DIV, POW, SQRT, ROOT, FACT, ABS and RAND functions
+    @date April 2021
+"""
+
 def ADD(x, y):
     """!
-    @brief Adds two numbers
-    @param x The first summand
-    @param y The second summand
-    @return return The sum
+        @brief Adds two numbers
+
+        @param x The first summand
+        @param y The second summand
+        @return return The sum
     """
     if (isinstance(x,(int,float))) and (isinstance(y,(int,float))):
         result = x + y
@@ -16,10 +24,11 @@ def ADD(x, y):
 
 def SUB(x, y):
     """!
-    @brief Subtracts y from x
-    @param x The minuend
-    @param y The subtrahend
-    @return result The difference
+        @brief Subtracts y from x
+
+        @param x The minuend
+        @param y The subtrahend
+        @return result The difference
     """
     if (isinstance(x,(int,float))) and (isinstance(y,(int,float))):
         result = x - y
@@ -29,10 +38,11 @@ def SUB(x, y):
 
 def MUL(x, y):
     """!
-    @brief Multiplies two numbers
-    @param x The multiplier
-    @param y The multiplicand
-    @return result The product
+        @brief Multiplies two numbers
+
+        @param x The multiplier
+        @param y The multiplicand
+        @return result The product
     """
     if (isinstance(x,(int,float))) and (isinstance(y,(int,float))):
         result = x * y
@@ -42,10 +52,11 @@ def MUL(x, y):
 
 def DIV(x, y):
     """!
-    @brief Divides x with y
-    @param x The divident
-    @param y The divisor
-    @return result The quotient
+        @brief Divides x with y
+
+        @param x The divident
+        @param y The divisor
+        @return result The quotient
     """
     if (isinstance(x,(int,float))) and (isinstance(y,(int,float))):
         if (x != 0) and (y != 0):
@@ -58,10 +69,11 @@ def DIV(x, y):
 
 def POW(x, y):
     """!
-    @brief Exponentiates x with y
-    @param x Number to exponentiate
-    @param y The exponent
-    @return result The power
+        @brief Exponentiates x with y
+
+        @param x Number to exponentiate
+        @param y The exponent
+        @return result The power
     """
     if (isinstance(x,(int,float))) and (isinstance(y,(int,float))):
         result = (x ** y)
@@ -71,9 +83,10 @@ def POW(x, y):
 
 def SQRT(x):
     """!
-    @brief Makes a square root of x
-    @param x The radicand
-    @return result
+        @brief Makes a square root of x
+
+        @param x The radicand
+        @return result
     """
     if (isinstance(x,(int,float))) and (isinstance(x,(int,float))):
         result = math.sqrt(x)
@@ -83,10 +96,11 @@ def SQRT(x):
 
 def ROOT(x, y):
     """!
-    @brief Exponentiates number
-    @param x The radicand
-    @param y The index
-    @return result 
+        @brief Exponentiates number
+
+        @param x The radicand
+        @param y The index
+        @return result 
     """
     if (isinstance(x,(int,float))) and (isinstance(y,(int,float))):
         if x < 0:
@@ -104,9 +118,10 @@ def ROOT(x, y):
 
 def FACT(x):
     """!
-    @brief Makes factorial of x
-    @param x Number to exponentiate
-    @return result 
+        @brief Makes factorial of x
+
+        @param x Number to exponentiate
+        @return result 
     """
     if (isinstance(x,(int,float))):
         if x >= 0:
@@ -120,9 +135,10 @@ def FACT(x):
 
 def ABS(x):
     """!
-    @brief Creates absolute value
-    @param x Number to be made absolute value of
-    @return result Absolute value
+        @brief Creates absolute value
+
+        @param x Number to be made absolute value of
+        @return result Absolute value
     """
     if (isinstance(x,(int,float))):
         if x >= 0:
@@ -134,11 +150,12 @@ def ABS(x):
 
 def RAND(x, y):
     """!
-    @brief RNG
-    Generates a random number from the <x, y> range 
-    @param x Lowest possible number
-    @param y Highest possible number
-    @return result The randomized number
+        @brief RNG
+
+        Generates a random number from the <x, y> range 
+        @param x Lowest possible number
+        @param y Highest possible number
+        @return result The randomized number
     """
     if (isinstance(x,(int))) and (isinstance(y,(int))): #no floating point
         if x < y:
